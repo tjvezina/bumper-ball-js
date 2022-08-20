c7921dab3209e5bf8e84175c2b816b57df7e462a
Tyler Vezina <tylerjvezina@gmail.com>
2022-08-19 22:46:11 -0400

    Imported assets from original project
    The original project used a .glf font file, which seems to be a binary file of C++ GLFONT struct data, which I have no way of importing - but I found the same font again online and just downloaded a .ttf

--- build/sketch.js
+++ build/sketch.js
@@ -1,11 +1,37 @@
+export const VIEW_WIDTH = 1024;
+export const VIEW_HEIGHT = 768;
+let viewScale = 1;
+let font;
+let imgChromeBall;
+let imgBackground;
+let imgLoading;
+let imgTitle;
+let imgPaddle;
+let imgPeg;
+let imgPegCracked1;
+let imgPegCracked2;
+globalThis.preload = function () {
+    loadFont('./assets/pericles-regular.ttf', result => { font = result; });
+    loadImage('./assets/chrome-ball.png', result => { imgChromeBall = result; });
+    loadImage('./assets/dark.png', result => { imgBackground = result; });
+    loadImage('./assets/loading.png', result => { imgLoading = result; });
+    loadImage('./assets/menu-title.png', result => { imgTitle = result; });
+    loadImage('./assets/paddle.png', result => { imgPaddle = result; });
+    loadImage('./assets/peg.png', result => { imgPeg = result; });
+    loadImage('./assets/peg-cracked-1.png', result => { imgPegCracked1 = result; });
+    loadImage('./assets/peg-cracked-2.png', result => { imgPegCracked2 = result; });
+};
 globalThis.windowResized = function () {
-    resizeCanvas(windowWidth, windowHeight);
+    viewScale = (windowWidth / windowHeight < VIEW_WIDTH / VIEW_HEIGHT ? windowWidth / VIEW_WIDTH : windowHeight / VIEW_HEIGHT);
+    resizeCanvas(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
 };
 globalThis.setup = function () {
-    createCanvas(windowWidth, windowHeight);
+    viewScale = (windowWidth / windowHeight < VIEW_WIDTH / VIEW_HEIGHT ? windowWidth / VIEW_WIDTH : windowHeight / VIEW_HEIGHT);
+    createCanvas(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
     pixelDensity(1);
 };
 globalThis.draw = function () {
     background(42);
+    scale(viewScale);
 };
 //# sourceMappingURL=sketch.js.map
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,12 +1,43 @@
+export const VIEW_WIDTH = 1024;
+export const VIEW_HEIGHT = 768;
+let viewScale = 1;
+
+let font: p5.Font;
+
+let imgChromeBall: p5.Image;
+let imgBackground: p5.Image;
+let imgLoading: p5.Image;
+let imgTitle: p5.Image;
+let imgPaddle: p5.Image;
+let imgPeg: p5.Image;
+let imgPegCracked1: p5.Image;
+let imgPegCracked2: p5.Image;
+
+globalThis.preload = function (): void {
+  loadFont('./assets/pericles-regular.ttf', result => { font = result; });
+
+  loadImage('./assets/chrome-ball.png', result => { imgChromeBall = result; });
+  loadImage('./assets/dark.png', result => { imgBackground = result; });
+  loadImage('./assets/loading.png', result => { imgLoading = result; });
+  loadImage('./assets/menu-title.png', result => { imgTitle = result; });
+  loadImage('./assets/paddle.png', result => { imgPaddle = result; });
+  loadImage('./assets/peg.png', result => { imgPeg = result; });
+  loadImage('./assets/peg-cracked-1.png', result => { imgPegCracked1 = result; });
+  loadImage('./assets/peg-cracked-2.png', result => { imgPegCracked2 = result; });
+};
+
 globalThis.windowResized = function (): void {
-  resizeCanvas(windowWidth, windowHeight);
+  viewScale = (windowWidth/windowHeight < VIEW_WIDTH/VIEW_HEIGHT ? windowWidth/VIEW_WIDTH : windowHeight/VIEW_HEIGHT);
+  resizeCanvas(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
 };
 
 globalThis.setup = function (): void {
-  createCanvas(windowWidth, windowHeight);
+  viewScale = (windowWidth/windowHeight < VIEW_WIDTH/VIEW_HEIGHT ? windowWidth/VIEW_WIDTH : windowHeight/VIEW_HEIGHT);
+  createCanvas(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
   pixelDensity(1);
 };
 
 globalThis.draw = function (): void {
   background(42);
+  scale(viewScale);
 };
